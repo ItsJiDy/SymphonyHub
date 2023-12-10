216127f3794a59697fb6a7c2243dd9249407bf15
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2023-12-10 09:05:57 +0800

    Update Executorwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww.lua

--- source/Executorwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww.lua
+++ source/Executorwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww.lua
@@ -1,1 +1,1 @@
-coroutine.wrap(loadstring(game:HttpGet("https://raw.githubusercontent.com/xwel33/hud/main/Notification"))().prompt)("Locked:","Symphony Hub [Executor Version] has already taken down!",10)
+coroutine.wrap(loadstring(game:HttpGet("https://raw.githubusercontent.com/xwel33/hud/main/Notification"))().prompt)("Locked:","Symphony Hub [Executor Version] has already taken down!",15)
