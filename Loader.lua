795b28452a9d692b3bceb5cd5fe59d0074f77dcf
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2024-03-13 18:56:40 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("This script is discontinued for now, we'll be back in a few months with more features and cool UI.")
+game.Players.LocalPlayer:Kick("This script is down in a few weeks/months, we'll be back in a few months with more features and cool UI.")
