7e123964548d8cd5572cf41255373ee46c6a2ca1
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2024-05-17 14:09:30 +0800

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Symphony Hub: This script is disabled and unable to use for now due to our API malfunctions.")
+game:GetService("Players").LocalPlayer:Kick("Symphony Hub: This script is disabled and unable to use for now due to our API malfunctions.")
